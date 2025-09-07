b34c35bc864e3501fb84ba71f790dc066ce4cd27
unknown <dangabarin2020@gmail.com>
2025-09-07 15:18:09 +0100

    update commit

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Lenis from 'lenis'
 import Landing from '@/components/Landing';
 import { AnimatePresence,} from 'framer-motion';
 import Preloader from '@/components/Preloader';
-import ZoomParallax from '@/components/Zoom';
+import Zoomfrom '@/components/Zoom';
 
 import Houses from '@/components/Houses';
 import Services from '@/components/Services';
@@ -43,7 +43,7 @@ export default function Home() {
     </AnimatePresence>
     
       <Landing />
-      <ZoomParallax />
+      <Zoom/>
       <Landing2 />
     <OurNumbers/>
     <Intro/>
